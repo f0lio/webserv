f3c23ffa865b7a08208c24822cc85553a47c8b4a
f0lio <36214338+f0lio@users.noreply.github.com>
2022-04-07 21:15:28 +0000

    make Console disablable from compiler flag

--- src/utils/Console.hpp
+++ src/utils/Console.hpp
@@ -2,7 +2,7 @@
 #pragma once
 
 #include <iostream>
-#include <stdlib.h>
+// #include <stdlib.h>
 
 class Console
 {
@@ -10,22 +10,24 @@ public:
     Console(){};
     ~Console(){};
 
-    void err(const std::string &msg, bool exit_ = false)
+    void err(const std::string &msg)
     {
+#ifndef CONSOLE_OFF
         std::cerr << "\033[1;31m" << msg << "\033[0m" << std::endl;
-        if (exit_)
-            exit(1);
+#endif
     }
 
     void warn(const std::string &msg)
     {
+#ifndef CONSOLE_OFF
         std::cerr << "\033[1;33m" << msg << "\033[0m" << std::endl;
+#endif
     }
 
     void log(const std::string &msg)
     {
+#ifndef CONSOLE_OFF
         std::cout << msg << std::endl;
+#endif
     }
 };
-
-
