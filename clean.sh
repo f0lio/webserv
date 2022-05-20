9d3bf5fa3a3eb3916abc5258727888599cceabce
ynejmi <na>
2022-05-20 13:03:47 +0100

    added clean.sh (tb3kik 2.0)

--- clean.sh
+++ clean.sh
@@ -1,13 +1,13 @@
-while true; do
+while [$1 != "-y"]; do
 	read -p "Do you wish to rm files specified in .gitignore? " yn
 	case $yn in
 		[Yy]* ) break;;
 		[Nn]* ) exit;;
-		* ) echo "yes for yes or no for no";;
+		* ) echo "    - (y)es for yes\n    - (n)o for no";;
 	esac
 done
 
 for f in $(cat .gitignore | grep -v "#"); do
 	rm -Rvf $f 2>/dev/null
 done
-echo "mchaaaw"
+echo "\nmchaaaw"
